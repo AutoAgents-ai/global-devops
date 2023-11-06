5300e178b5a6d9e91275fcf904ba0fe4ef430b1c
zoupengfei <pengfei.zou@autoagents.ai>
2023-11-06 13:18:18 +0800

    init

---
+++ nginx/build.sh
@@ -0,0 +1,5 @@
+IMAGE_REPOSITORY_URL=150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents
+IMAGE_TAG=nginx-master
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $IMAGE_REPOSITORY_URL
+docker build -t $IMAGE_REPOSITORY_URL:$IMAGE_TAG .
+docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
