47ad0973baae89168ae0ef93e7e8cf95a7e190ee
zoupengfei <pengfei.zou@autoagents.ai>
2023-11-07 15:20:03 +0800

    build.sh

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,5 +1,7 @@
 IMAGE_REPOSITORY_URL=150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents
 IMAGE_TAG=nginx-master
+git fetch --all
+git pull
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $IMAGE_REPOSITORY_URL
 docker build  --platform linux/amd64 -t $IMAGE_REPOSITORY_URL:$IMAGE_TAG .
 docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
