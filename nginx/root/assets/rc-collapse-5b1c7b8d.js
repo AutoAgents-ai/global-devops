10008ca44a76504e6c9365835965bbf3a38922cc
zoupengfei <pengfei.zou@autoagents.ai>
2023-11-07 15:02:10 +0800

    update server.sh

---
+++ nginx/root/assets/rc-collapse-5b1c7b8d.js
@@ -0,0 +1,1 @@
+import"./classnames-25f5e917.js";import"./react-f8c4b268.js";import"./rc-motion-6af00954.js";import"./react-is-e8e5dbb3.js";import"./rc-util-35a1817f.js";
---
+++ nginx/root/assets/rc-steps-d7b6ecf3.js
@@ -0,0 +1,1 @@
+import"./react-f8c4b268.js";import"./classnames-25f5e917.js";
