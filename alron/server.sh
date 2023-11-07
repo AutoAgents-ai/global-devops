dde7bc5cadfecc83c14657a321001b4ccb8fd794
zoupengfei <pengfei.zou@autoagents.ai>
2023-11-07 18:00:28 +0800

    build script modify

--- alron/server.sh
+++ alron/server.sh
@@ -1,9 +1,9 @@
 current_dir=$(cd $(dirname $0); pwd)
 cd $current_dir
-mkdir mysql redis alron
 sudo sh ./awslogin.sh
 sudo docker pull 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:nginx-master
 sudo docker pull 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:alron-master
+sudo docker pull 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:taskrunner-master
 sudo docker-compose --env-file "$1.env" stop
 sudo docker-compose --env-file "$1.env" rm
 sudo docker-compose --env-file "$1.env" up -d
