e1da1c59d2ca2fe05fe77300f1840984d4caa408
zoupengfei <pengfei.zou@autoagents.ai>
2023-12-15 21:49:20 +0800

    front

--- tiangong/nginx/frontpages/static/index-19864715.js
+++
@@ -1,1 +0,0 @@
-import{H as t,aJ as n,l as a,m as r,n as c}from"./vue-82a6b7fc.js";import{u as i,f as p}from"./index-efc4a172.js";import"./element-4990942a.js";import"./vxe-45effba2.js";const T=t({__name:"index",setup(m){const{ssoToken:o,goTg:e}=i(),s=n();return(async()=>{await c(),p(),o().then(()=>{s.push({path:"/"})}).catch(()=>{e()})})(),(_,f)=>(a(),r("div"))}});export{T as default};
