c1521348c5f8a185f47192c938cdb28eb7f9574e
zoupengfei <pengfei.zou@autoagents.ai>
2023-12-16 14:36:04 +0800

    abc

--- mindsynth/server.sh
+++
@@ -1,6 +0,0 @@
-current_dir=$(cd $(dirname $0); pwd)
-cd $current_dir
-sudo sh ./awslogin.sh
-sudo docker stop 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:mindsynth-main
-sudo docker pull 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:mindsynth-main
-sudo docker run 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:mindsynth-main
--- tiangong/nginx/nginx_srv.sh
+++
@@ -1,7 +0,0 @@
-current_dir=$(cd $(dirname $0); pwd)
-cd $current_dir
-sudo sh ./awslogin.sh
-sudo docker pull 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:mindsynth-nginx
-sudo docker stop nginx
-sudo docker remove nginx
-sudo docker run -p 80:80 -d --name nginx -e SPRING_PROFILES_ACTIVE=test 150128700443.dkr.ecr.us-east-1.amazonaws.com/autoagents:mindsynth-nginx 
