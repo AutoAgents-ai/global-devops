a98799091a11a953e7123ebbac3c037b1e93b025
haoyang <haoyang@bodypark.ai>
2023-12-18 14:31:19 +0800

    fix bug

---
+++ tiangong/nginx/frontpages/static/index-e899a726.js
@@ -0,0 +1,1 @@
+import{H as t,aG as r,aJ as a,l as o,m as s}from"./vue-3e7b265f.js";const _=t({__name:"index",setup(n){const e=r();return a().replace({path:"/"+e.params.path,query:e.query}),(c,p)=>(o(),s("div"))}});export{_ as default};
---
+++ tiangong/nginx/frontpages/static/steer-a76c52ec.js
@@ -0,0 +1,1 @@
+import{r}from"./index-92377631.js";function o(e){return r({url:`/auth/user/keys?token=${e}`,method:"post"})}function n(e){return r({url:`/balance?token=${e}`,method:"get"})}export{o as l,n as r};
