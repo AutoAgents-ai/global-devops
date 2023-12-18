4e9d3ad9140b677a2ab8a443b325d6b272725b8d
zoupengfei <pengfei.zou@autoagents.ai>
2023-12-18 18:54:25 +0800

    yifangda

---
+++ yifangda/nginx/build.sh
@@ -0,0 +1,11 @@
+IMAGE_REPOSITORY_HOST=registry.cn-beijing.aliyuncs.com
+IMAGE_REPOSITORY_URL=registry.cn-beijing.aliyuncs.com/autoagents/libs
+IMAGE_TAG=mindsynth-nginx-yifangda
+git fetch --all
+git pull
+git add .
+git commit -m "frontend"
+git push -u origin main
+docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagentslibs123
+docker build  --platform linux/amd64 -t $IMAGE_REPOSITORY_URL:$IMAGE_TAG .
+docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
---
+++ yifangda/start.sh
@@ -0,0 +1,10 @@
+current_dir=$(cd $(dirname $0); pwd)
+IMAGE_REPOSITORY_HOST=registry.cn-beijing.aliyuncs.com
+IMAGE_REPOSITORY_URL=registry.cn-beijing.aliyuncs.com/autoagents/libs
+
+sudo cd $current_dir
+sudo docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagentslibs123
+sudo docker pull $IMAGE_REPOSITORY_URL:mindsynth-nginx-yifangda
+sudo docker pull $IMAGE_REPOSITORY_URL:mindsynth-main-yifangda
+sudo docker compose  rm -f -s
+sudo docker compose  up -d
