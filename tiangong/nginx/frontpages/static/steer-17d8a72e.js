519adadc6124eceb326844c4be30bb77021ef471
haoyang <haoyang@bodypark.ai>
2023-12-18 18:17:35 +0800

    fix bug

--- tiangong/nginx/frontpages/static/steer-17d8a72e.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-da45b20f.js";function o(e){return r({url:`/auth/user/keys?token=${e}`,method:"post"})}function n(e){return r({url:`/balance?token=${e}`,method:"get"})}export{o as l,n as r};
