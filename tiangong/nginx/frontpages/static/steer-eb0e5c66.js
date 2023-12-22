b6cd648de600fe24d6ba3a440e62d1d883b0b1ce
linguohua <L15753706616@163.com>
2023-12-22 19:06:04 +0800

    lgh-test

---
+++ tiangong/nginx/frontpages/static/steer-eb0e5c66.js
@@ -0,0 +1,1 @@
+import{r}from"./index-71f8a02e.js";function o(e){return r({url:`/auth/user/keys?token=${e}`,method:"post"})}function n(e){return r({url:`/chat/balance?token=${e}`,method:"get"})}export{o as l,n as r};
