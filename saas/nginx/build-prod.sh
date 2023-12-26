6cdb5e3421605201f0a4b0ccab31f6c3e4540fc9
haoyang <haoyang@bodypark.ai>
2023-12-26 17:21:34 +0800

    update sass config

--- saas/nginx/build-prod.sh
+++ saas/nginx/build-prod.sh
@@ -1,11 +1,11 @@
-IMAGE_REPOSITORY_HOST=registry.cn-beijing.aliyuncs.com
-IMAGE_REPOSITORY_URL=registry.cn-beijing.aliyuncs.com/autoagents/libs
-IMAGE_TAG=mindsynth-nginx-prod
+IMAGE_REPOSITORY_HOST=autoagents-registry.cn-beijing.cr.aliyuncs.com
+IMAGE_REPOSITORY_URL=autoagents-registry.cn-beijing.cr.aliyuncs.com/agent/mind
+IMAGE_TAG=nginx-sass
 git fetch --all
 git pull
 git add .
 git commit -m "frontend"
 git push -u origin main
-docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagentslibs123
+docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagents2023
 docker build  --platform linux/amd64 -t $IMAGE_REPOSITORY_URL:$IMAGE_TAG .
 docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
--- saas/start.sh
+++ saas/start.sh
@@ -1,10 +1,10 @@
 current_dir=$(cd $(dirname $0); pwd)
-IMAGE_REPOSITORY_HOST=registry.cn-beijing.aliyuncs.com
-IMAGE_REPOSITORY_URL=registry.cn-beijing.aliyuncs.com/autoagents/libs
+IMAGE_REPOSITORY_HOST=autoagents-registry.cn-beijing.cr.aliyuncs.com
+IMAGE_REPOSITORY_URL=autoagents-registry.cn-beijing.cr.aliyuncs.com/agent/mind
 
 sudo cd $current_dir
-sudo docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagentslibs123
-sudo docker pull $IMAGE_REPOSITORY_URL:mindsynth-nginx
-sudo docker pull $IMAGE_REPOSITORY_URL:mindsynth-main
+sudo docker login $IMAGE_REPOSITORY_HOST --username autoagents --password autoagents2023
+sudo docker pull $IMAGE_REPOSITORY_URL:nginx-sass
+sudo docker pull $IMAGE_REPOSITORY_URL:mindsynth-sass
 sudo docker compose  rm -f -s
 sudo docker compose  up -d
