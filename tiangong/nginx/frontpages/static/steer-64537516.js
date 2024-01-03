afff63773cd0aa8dbde40b91948a5e971257b87c
linguohua <L15753706616@163.com>
2024-01-03 14:48:41 +0800

    lgh-logout

--- tiangong/nginx/frontpages/static/steer-64537516.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-2f6ea3a3.js";function o(e){return t({url:`/auth/user/keys?token=${e}`,method:"post"})}function n(e){return t({url:`/chat/balance?token=${e}`,method:"get"})}export{o as l,n as r};
